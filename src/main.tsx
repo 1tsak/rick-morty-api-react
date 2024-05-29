662633d67bd6de52b5acc93eff34867393a60fab
Aakash Jha <hey.a2v10@gmail.com>
2024-05-29 11:49:31 +0530

    Added Routing for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './Home/index.tsx'
+import Episode from './Episode/index.tsx'
+import Character from './Character/index.tsx'
+import Search from './Search/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Home/>
+  },
+  {
+    path:"/episode/:episodeID",
+    element: <Episode/>
+  },
+  {
+    path:"/character/:characterID",
+    element: <Character/>
+  },
+  {
+    path:"/search",
+    element: <Search/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
