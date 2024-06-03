ca34da2ee1c3d2a318643e0ff2ecb6a7b402c4cd
Aakash Jha <hey.a2v10@gmail.com>
2024-06-03 11:06:23 +0530

    Completed Episode & Location Card

--- src/Home/components/LocationCard/index.tsx
+++ src/Home/components/LocationCard/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Location } from '../../../utils/types'
+import { IoLocationSharp } from 'react-icons/io5'
+import { Link } from 'react-router-dom'
+import { FaChevronRight } from 'react-icons/fa'
 
-const LocationCard:React.FC<Location> = ({id,name,created,dimension,type}) => {
+const LocationCard:React.FC<Location> = ({id,name,type,dimension}) => {
   return (
-    <div className="shadow-md rounded-lg bg-[#3C3E44] flex w-[600px] h-[220px] overflow-hidden object-cover m-auto">
-      {name}
+    <div className="shadow-md rounded-lg bg-[#3C3E44] flex items-center p-5 gap-5 w-[600px] h-[220px] overflow-hidden object-cover m-auto">
+      <IoLocationSharp  color="#2e2e2e" size={100} />
+      <div className="text-white flex flex-col flex-1">
+        <h2 className="text-slate-400 text-sm">{dimension}</h2>
+        <Link to={`/episode/${id}`}>
+          <h2 className="text-xl">{name}</h2>
+        </Link>
+        <h2 className="mt-2 text-sm text-slate-400">Type:</h2>
+        <h2>{type}</h2>
+      </div>
+      <Link to={`/location/${id}`}>
+        <FaChevronRight size={35} color="#cdcdcd" />
+      </Link>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,28 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-      <nav className="flex px-5 py-2 items-center justify-between">
-        <Logo />
-        <div className="shadow-sm bg-slate-100 px-8 rounded-3xl">
-          <input
-            className="outline-none border-none focus:border-none focus:outline-none bg-transparent w-[400px] h-[50px] "
-            placeholder="Search for your favourite character.."
-            type="text"
-          />
-        </div>
-        <div></div>
-      </nav>
+    <nav className="flex px-5 py-4 items-center justify-between">
+      <Logo />
+      <div className="shadow-sm bg-slate-100 px-4  rounded-3xl">
+        <select
+          className=" bg-transparent text-slate-600 px-1 py-2 border-r-[16px] border-transparent text-sm outline-none cursor-pointer"
+          title="Select Type"
+          name="type"
+          id="select-type"
+          // onChange={handleSelectChange}
+        >
+          <option value="characters">Characters</option>
+          <option value="episodes">Episodes</option>
+          <option value="locations">Locations</option>
+        </select>
+        <input
+          className="outline-none border-none focus:border-none focus:outline-none bg-transparent w-[400px] h-[50px] "
+          placeholder="Search for your favourite character.."
+          type="text"
+        />
+      </div>
+      <div></div>
+    </nav>
   );
 };
 
