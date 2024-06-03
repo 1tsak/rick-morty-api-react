2571b265b0669718d1dc8d1f846eaa9deb12de8d
Aakash Jha <hey.a2v10@gmail.com>
2024-06-04 00:56:35 +0530

    fixed design

--- src/Home/components/ItemList/index.tsx
+++ src/Home/components/ItemList/index.tsx
@@ -3,15 +3,17 @@ import { ItemListType } from "../../../utils/types";
 const ItemList = ({ itemList,itemComponent:ItemCard }:ItemListType) => {
     
   return (
-    <div className=" grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 min-w-[1225px] gap-6 ">
+    <div className="min-h-[300px]">
       {itemList && itemList.length > 0 ? (
-        itemList.map((character: any) => (
+        <div className=" grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 min-w-[1225px]  gap-6 ">
+        {itemList.map((character: any) => (
           <ItemCard {...character} key={character.id} />
-        ))
+        ))}
+        </div>
       ) : (
-        <>No Characters</>
+        <div className="text-center text-white">No Items</div>
       )}
-    </div>
+      </div>
   );
 };
 
