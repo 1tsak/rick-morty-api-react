033c1a816c08d6fdd8716de8702aefae9654b62e
Aakash Jha <hey.a2v10@gmail.com>
2024-06-05 17:23:11 +0530

    Added Back Button

--- src/Search/index.tsx
+++ src/Search/index.tsx
@@ -4,6 +4,8 @@ import CharacterCard from "../Home/components/CharacterCard";
 import EpisodeCard from "../Home/components/EpisodeCard";
 import LocationCard from "../Home/components/LocationCard";
 import Loading from "../components/Loading";
+import { Link } from "react-router-dom";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const Search = () => {
   const { results, loading, type } = useSelector(
@@ -21,7 +23,18 @@ const Search = () => {
 
   return (
     <div className="search-results bg-[#272B33] h-screen overflow-hidden flex flex-col p-2 items-center justify-center">
+     
+     <div className="flex w-full items-center justify-between p-5">
+      <Link className="w-fit" to={"/"}>
+        <IoMdArrowRoundBack
+          className="cursor-pointer"
+          color="white"
+          size={35}
+        />
+      </Link>
       <h2 className="text-white m-2">Search Results</h2>
+      <div></div>
+      </div> 
       {results && results.length > 0 ? (
         <div className="h-screen overflow-y-auto">
           <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 min-w-[1225px] gap-6">
